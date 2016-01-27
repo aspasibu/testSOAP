a4aae653a565a11b6a9c249f7a525a2223425426
Aspasibu <andrey.spasibukhov@yandex.ru>
2016-01-27 16:08:31 +0300

    changes for HosService

--- testSOAP/src/main/java/com/tsystems/logitest/repository/AuthRepository.java
+++ testSOAP/src/main/java/com/tsystems/logitest/repository/AuthRepository.java
@@ -16,7 +16,7 @@ public interface AuthRepository extends JpaRepository<AuthEvents, Long> {
 	@Query("select e from AuthEvents e where e.driver=:driver order by e.date desc")
 	List<AuthEvents> getEventsByDriver(@Param("driver") Driver driver, Pageable pageable);
 
-	@Query("select e,d from AuthEvents e RIGHT OUTER join e.driver d where d.userName=:userName and e.date between :startPeriod and :endPeriod order by e.date asc")
+	@Query("select e,d from AuthEvents e join e.driver d where d.userName=:userName and e.date between :startPeriod and :endPeriod order by e.date asc")
 	List<AuthEvents> getEventsForPeriod(@Param("userName") String userName, @Param("startPeriod") Date startPeriod,
 			@Param("endPeriod") Date endPeriod);
 }
--- testSOAP/src/main/java/com/tsystems/logitest/service/impl/HosServiceImpl.java
+++ testSOAP/src/main/java/com/tsystems/logitest/service/impl/HosServiceImpl.java
@@ -1,18 +1,25 @@
 package com.tsystems.logitest.service.impl;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tsystems.logitest.entity.AuthEvents;
+import com.tsystems.logitest.entity.Driver;
 import com.tsystems.logitest.repository.AuthRepository;
 import com.tsystems.logitest.repository.DriverRepository;
 import com.tsystems.logitest.service.HosService;
 
 public class HosServiceImpl implements HosService {
 
-	private String responseMeassge = "%SURNAME, %NAME for a period %START - %END";
+	// private StringBuilder responseMeassge = new StringBuilder("%SURNAME,
+	// %NAME for a period %START - %END: %TOTAL");
+
+	private final String RESPONSE_USER_NOT_FOUND = "USER NOT FOUND";
+
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
 
 	@Autowired
 	private AuthRepository authEventsRepository;
@@ -23,13 +30,35 @@ public class HosServiceImpl implements HosService {
 	@Override
 	public String calculate(String username, Date startPeriod, Date endPeriod) {
 
-		List<AuthEvents> events = authEventsRepository.getEventsForPeriod(username, startPeriod, endPeriod);
+		Driver driver = driverRepository.findByUserName(username);
 
-		if (events == null || events.size() == 0) {
-			return null;
+		if (driverRepository.findByUserName(username) == null) {
+			return RESPONSE_USER_NOT_FOUND;
 		}
 
-		return null;
+		List<AuthEvents> events = authEventsRepository.getEventsForPeriod(username, startPeriod, endPeriod);
+
+		Date total = new Date(0);
+
+		if (events != null || events.size() > 0) {
+
+		}
+
+		return getResponseMessage(driver, startPeriod, endPeriod, total.getTime());
+	}
+
+	private String getResponseMessage(Driver driver, Date startPeriod, Date endPeriod, long total) {
+		StringBuilder responseMeassge = new StringBuilder();
+		responseMeassge.append(driver.getSurname());
+		responseMeassge.append(", ");
+		responseMeassge.append(driver.getName());
+		responseMeassge.append(" for a period ");
+		responseMeassge.append(dateFormat.format(startPeriod));
+		responseMeassge.append(" - ");
+		responseMeassge.append(dateFormat.format(endPeriod));
+		responseMeassge.append(": ");
+		responseMeassge.append(String.valueOf(total));
+		return responseMeassge.toString();
 	}
 
 }
