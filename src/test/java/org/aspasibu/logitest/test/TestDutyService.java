635e927097fe6f7cfb7e50ef401b82667df5d4ac
Aspasibu <andrey.spasibukhov@yandex.ru>
2016-02-01 11:55:13 +0300

    maven build plugin

--- src/test/java/org/aspasibu/logitest/test/TestDutyService.java
+++ src/test/java/org/aspasibu/logitest/test/TestDutyService.java
@@ -1,13 +1,10 @@
 package org.aspasibu.logitest.test;
 
-import static org.junit.Assert.*;
-
 import static org.easymock.EasyMock.createNiceMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
-import static org.easymock.EasyMock.expectLastCall;
-import static org.easymock.EasyMock.isA;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
@@ -21,12 +18,9 @@ import org.aspasibu.logitest.repository.DutyRepository;
 import org.aspasibu.logitest.service.DutyService;
 import org.aspasibu.logitest.service.impl.DutyServiceImpl;
 import org.aspasibu.logitest.types.DutyResponseType;
-import org.easymock.EasyMock;
-import org.hamcrest.core.IsAnything;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.util.ReflectionTestUtils;
 
 public class TestDutyService {
@@ -49,28 +43,28 @@ public class TestDutyService {
 		dutyService = new DutyServiceImpl();
 		ReflectionTestUtils.setField(dutyService, "driverRepository", driverRepository);
 		ReflectionTestUtils.setField(dutyService, "dutyEventsRepository", dutyRepository);
-		
-		driver = new Driver("","",USER_NAME,password);
+
+		driver = new Driver("", "", USER_NAME, password);
 		login = new DutyEvents();
 		login.setType(EventType.LOGIN);
 		logout = new DutyEvents();
 		logout.setType(EventType.LOGOUT);
-		
+
 		expect(driverRepository.findByUserName(USER_NAME)).andReturn(driver).anyTimes();
 		replay(driverRepository);
 	}
 
 	@Test
-	public void testLogIn() {							
-		
+	public void testLogIn() {
+
 		List<DutyEvents> events = new ArrayList<>();
 		events.add(login);
-		
+
 		expect(dutyRepository.getEventsByDriver(driver, new PageRequest(0, 1))).andReturn(null);
 		expect(dutyRepository.getEventsByDriver(driver, new PageRequest(0, 1))).andReturn(events);
 		expect(dutyRepository.saveAndFlush(isA(DutyEvents.class))).andReturn(new DutyEvents());
 		replay(dutyRepository);
-		
+
 		assertEquals("test login", dutyService.logIn(null, password), DutyResponseType.USER_NOT_FOUND);
 		assertEquals("test login", dutyService.logIn(USER_NAME, null), DutyResponseType.WRONG_PASSWORD);
 		assertEquals("test login", dutyService.logIn(USER_NAME, password), DutyResponseType.SUCCESSFULL);
@@ -80,15 +74,15 @@ public class TestDutyService {
 	}
 
 	@Test
-	public void testLogOut() {	
+	public void testLogOut() {
 		List<DutyEvents> events = new ArrayList<>();
 		events.add(logout);
-		
+
 		expect(dutyRepository.getEventsByDriver(driver, new PageRequest(0, 1))).andReturn(null);
 		expect(dutyRepository.getEventsByDriver(driver, new PageRequest(0, 1))).andReturn(events);
 		expect(dutyRepository.saveAndFlush(isA(DutyEvents.class))).andReturn(new DutyEvents());
 		replay(dutyRepository);
-		
+
 		assertEquals("test logout", dutyService.logOut(null), DutyResponseType.USER_NOT_FOUND);
 		assertEquals("test logout", dutyService.logOut(USER_NAME), DutyResponseType.SUCCESSFULL);
 		assertEquals("test logout", dutyService.logOut(USER_NAME), DutyResponseType.SECOND_ATTEMPT_LOGOUT);
